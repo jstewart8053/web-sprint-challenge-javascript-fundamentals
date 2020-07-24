1d336ab7220e2783d3b35f61c73096b66500d314
Jennifer Stewart <jstewart8053@gmail.com>
2020-07-24 12:46:34 -0400

    closure completed

--- challenges/closure.js
+++ challenges/closure.js
@@ -6,19 +6,24 @@
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+    console.log(external);
+    const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
+    function nestedFunction() {
+        console.log(internal);
+    };
+    nestedFunction();
 }
 myFunction();
 
 // Explanation: 
-
+//The nestedFunction can access the internal variable because it is located within a closure. A function is nested when created inside another function.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+    let arr = Array.from({ length: num }, (v, k) => k + 1);
+    return arr.reduce((a, b) => a + b, 0)
+}
